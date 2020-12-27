888b70e281bf456703b1488681c3878f8e42460d
Pierre-Antoine Passet <pa@passet.org>
2020-12-27 17:38:18 +0100

    Adds Vec3 module

---
+++ src/vec3.rs
@@ -0,0 +1,212 @@
+use std::ops::{Add, Div, Mul, Neg, Sub};
+
+pub struct Vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
+        Vec3 { x, y, z }
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn unit_vector(&self) -> Vec3 {
+        self / self.length()
+    }
+
+    pub fn dot(&self, other: &Vec3) -> f64 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    pub fn cross(&self, other: &Vec3) -> Vec3 {
+        Vec3 {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+}
+
+impl Add for &Vec3 {
+    type Output = Vec3;
+
+    fn add(self, other: &Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl Sub for &Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, other: &Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+impl Neg for &Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Vec3 {
+        Vec3 {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
+impl Mul<f64> for &Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, k: f64) -> Vec3 {
+        Vec3 {
+            x: k * self.x,
+            y: k * self.y,
+            z: k * self.z,
+        }
+    }
+}
+
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, v: Vec3) -> Vec3 {
+        Vec3 {
+            x: self * v.x,
+            y: self * v.y,
+            z: self * v.z,
+        }
+    }
+}
+
+impl Div<f64> for &Vec3 {
+    type Output = Vec3;
+
+    fn div(self, k: f64) -> Vec3 {
+        Vec3 {
+            x: self.x / k,
+            y: self.y / k,
+            z: self.z / k,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let v2 = Vec3::new(4.0, 5.0, 6.0);
+        let v3 = &v1 + &v2;
+        assert_eq!(v3.x, 5.0);
+        assert_eq!(v3.y, 7.0);
+        assert_eq!(v3.z, 9.0);
+    }
+
+    #[test]
+    fn test_sub() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let v2 = Vec3::new(4.0, 1.0, 4.0);
+        let v3 = &v1 - &v2;
+        assert_eq!(v3.x, -3.0);
+        assert_eq!(v3.y, 1.0);
+        assert_eq!(v3.z, -1.0);
+    }
+
+    #[test]
+    fn test_neg() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let v3 = -&v1;
+        assert_eq!(v3.x, -1.0);
+        assert_eq!(v3.y, -2.0);
+        assert_eq!(v3.z, -3.0);
+    }
+
+    #[test]
+    fn test_mul_vec3() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let k = 3.0;
+        let v3 = &v1 * k;
+        assert_eq!(v3.x, 3.0);
+        assert_eq!(v3.y, 6.0);
+        assert_eq!(v3.z, 9.0);
+    }
+
+    #[test]
+    fn test_mul_f64() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let k = 3.0;
+        let v3 = k * v1;
+        assert_eq!(v3.x, 3.0);
+        assert_eq!(v3.y, 6.0);
+        assert_eq!(v3.z, 9.0);
+    }
+
+    #[test]
+    fn test_div_f64() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let k = 2.0;
+        let v3 = &v1 / k;
+        assert_eq!(v3.x, 0.5);
+        assert_eq!(v3.y, 1.0);
+        assert_eq!(v3.z, 1.5);
+    }
+
+    #[test]
+    fn test_length_squared() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        assert_eq!(v1.length_squared(), 14.0);
+    }
+
+    #[test]
+    fn test_length() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let len_squared = v1.length_squared();
+        assert_eq!(v1.length(), len_squared.sqrt());
+    }
+
+    #[test]
+    fn unit_vector() {
+        let v1 = Vec3::new(2.0, 0.0, 0.0);
+        let v2 = v1.unit_vector();
+        assert_eq!(v2.x, 1.0);
+        assert_eq!(v2.y, 0.0);
+        assert_eq!(v2.z, 0.0);
+    }
+
+    #[test]
+    fn dot() {
+        let v1 = Vec3::new(1.0, 2.0, -3.0);
+        let v2 = Vec3::new(4.0, -5.0, 6.0);
+        assert_eq!(v1.dot(&v2), -24.0);
+    }
+
+    #[test]
+    fn cross() {
+        let v1 = Vec3::new(1.0, 2.0, -3.0);
+        let v2 = Vec3::new(4.0, -5.0, 6.0);
+        let v3 = v1.cross(&v2);
+        assert_eq!(v3.x, -3.0);
+        assert_eq!(v3.y, -18.0);
+        assert_eq!(v3.z, -13.0);
+    }
+}
